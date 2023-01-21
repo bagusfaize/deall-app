02acd9de5ccc13144149da5815b12a057c5f1f45
Bagus Faizun <bagus.noorfaizun@dansmultipro.com>
2023-01-22 04:34:11 +0700

    add table component

---
+++ components/Table.js
@@ -0,0 +1,76 @@
+import styles from '@/styles/components/Table.module.css'
+import { useEffect, useState } from 'react'
+import Pagination from './Pagination'
+import { AiOutlineFileSearch } from 'react-icons/ai'
+
+export default function Table({ columns, data, limit = 10, total }) {
+    const [dataDisplayed, setDataDisplayed] = useState([])
+    const [page, setPage] = useState(1);
+    const dataNotFound = data.length === 0;
+
+    useEffect(() => {
+        setDataDisplayed(paginate(data, page, limit))
+    }, [data, limit, page])
+
+    useEffect(() => {
+        setPage(1)
+    }, [data])
+
+    const paginate = (items, page, perPage) => items.slice(perPage * (page - 1), perPage * page);
+
+    const handlePage = (type) => {
+        if (type === 'prev' && page > 1) {
+            setPage(page - 1)
+        } else if (type === 'next') {
+            setPage(page + 1)
+        }
+    }
+
+    const generateEmptyState = () => {
+        return(
+            <div className={styles.emptyState}>
+               <div>Data Not Found</div>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <table className={styles.generalTable}>
+                <thead>
+                    <tr>
+                        {columns.map(item => {
+                            if (typeof item === 'object') {
+                                return <th key={`th-${item.title}`} width={item.width}>{item.title}</th>
+                            } else {
+                                return <th key={`th-${item}`}>{item}</th>
+
+                            }
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {dataDisplayed.map(item =>
+                    (
+                        <tr key={`td-${item}`}>
+                            {item.map(value => (
+                                <td key={`td-${value}`}>{value}</td>
+                            ))}
+                        </tr>
+                    ))
+                    }
+                </tbody>
+            </table>
+            {dataNotFound && generateEmptyState()}
+            <div className={styles.pagination}>
+                <Pagination
+                    handlePage={handlePage}
+                    page={page}
+                    limit={limit}
+                    productLength={dataDisplayed.length}
+                    totalData={total}
+                />
+            </div>
+        </>
+    )
+}
