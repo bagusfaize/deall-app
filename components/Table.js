f39988797537db35c65d6b73e464e625069c63f0
Bagus Faizun <bagus.noorfaizun@dansmultipro.com>
2023-02-21 17:05:14 +0700

    refactor table component

--- components/Table.js
+++ components/Table.js
@@ -1,30 +1,10 @@
 import styles from '@/styles/components/Table.module.css'
-import { useEffect, useState } from 'react'
-import Pagination from './Pagination'
-import { AiOutlineFileSearch } from 'react-icons/ai'
+import { useSelector } from 'react-redux';
 
-export default function Table({ columns, data, limit = 10, total }) {
-    const [dataDisplayed, setDataDisplayed] = useState([])
-    const [page, setPage] = useState(1);
+export default function Table({ columns, data }) {
+    const generalState = useSelector(state => state.general);
+    const isLoading = generalState.isLoading;
     const dataNotFound = data.length === 0;
-
-    useEffect(() => {
-        setDataDisplayed(paginate(data, page, limit))
-    }, [data, limit, page])
-
-    useEffect(() => {
-        setPage(1)
-    }, [data])
-
-    const paginate = (items, page, perPage) => items.slice(perPage * (page - 1), perPage * page);
-
-    const handlePage = (type) => {
-        if (type === 'prev' && page > 1) {
-            setPage(page - 1)
-        } else if (type === 'next') {
-            setPage(page + 1)
-        }
-    }
 
     const generateEmptyState = () => {
         return(
@@ -50,27 +30,18 @@ export default function Table({ columns, data, limit = 10, total }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {dataDisplayed.map(item => {
+                    {data.map((item, index) => {
                     return (
-                        <tr key={`tr-${item[0]}`}>
-                            {item.map(value => (
-                                <td key={`td-${item[0]}`}>{value}</td>
+                        <tr key={`tr-${index}`}>
+                            {item.map((value, index2) => (
+                                <td key={`td-${index}-${index2}`}>{value}</td>
                             ))}
                         </tr>
                     )})
                     }
                 </tbody>
             </table>
-            {dataNotFound && generateEmptyState()}
-            <div className={styles.pagination}>
-                <Pagination
-                    handlePage={handlePage}
-                    page={page}
-                    limit={limit}
-                    productLength={dataDisplayed.length}
-                    totalData={total}
-                />
-            </div>
+            {dataNotFound && !isLoading && generateEmptyState()}
         </>
     )
 }
