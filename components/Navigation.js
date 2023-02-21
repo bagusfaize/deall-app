853ecc4aabe38e6a6868498c1b0ef0cbd5c2e32c
Bagus Faizun <bagus.noorfaizun@dansmultipro.com>
2023-02-21 17:02:40 +0700

    refactor navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,40 +1,41 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from '@/styles/components/Navigation.module.css';
-import { MdOutlineListAlt, MdOutlineAccountCircle } from 'react-icons/md';
+import { MdOutlineListAlt } from 'react-icons/md';
 import { FiShoppingBag } from 'react-icons/fi';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 import { useSelector } from 'react-redux';
 
 export default function Navigation() {
     const cart = useSelector(state => state.cart);
     const router = useRouter();
     const pathname = router.pathname;
-    const isCartEmpty = cart.totalQty === 0;
+    const isCartEmpty = cart.total === 0;
 
     return (
         <div className={styles.navigation}>
             <div className={styles.brand}>
                 <span className={styles.logo}>D</span>
-                <span>DeallCommurz</span>
+                <span>Deall Commerce</span>
             </div>
             <ul className={styles.menu}>
                 <Link href="/">
                     <li className={pathname === '/' ? styles.isActive : null}>
                         <span className={styles.icon}><MdOutlineListAlt/></span>
-                        <span className={styles.title}>Product</span>
-                     </li>
+                        <span className={styles.title}>Products</span>
+                    </li>
                 </Link>
                 <Link href="/cart">
-                    <li className={pathname === '/cart' ? styles.isActive : null}>
+                    <li className={pathname.includes('/cart') ? styles.isActive : null}>
                         <span className={styles.icon}><FiShoppingBag/></span>
-                        <span className={styles.title}>Cart</span>
-                        { !isCartEmpty && <span className={styles.counter}>{cart.totalQty}</span> }
+                        <span className={styles.title}>Order Cart</span>
+                        { !isCartEmpty && <span className={styles.counter}>{cart.total}</span> }
                     </li>
                 </Link>
                 <Link href="/account">
                     <li className={pathname === '/account' ? styles.isActive : null}>
-                        <span className={styles.icon}><MdOutlineAccountCircle/></span>
-                        <span className={styles.title}>Account</span>
+                        <span className={styles.icon}><AiOutlineInfoCircle/></span>
+                        <span className={styles.title}>About</span>
                     </li>
                 </Link>
             </ul>
