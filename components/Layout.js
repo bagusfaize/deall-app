df851bebf56205ec4f84c7cec58faac1a2eeb876
Bagus Faizun <bagus.noorfaizun@dansmultipro.com>
2023-02-21 17:02:26 +0700

    add loading screen

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,19 @@
 import styles from '@/styles/components/Layout.module.css'
+import { useSelector } from 'react-redux';
 import Navigation from './Navigation';
 
 export default function Layout(props) {
-    const { children } = props;
+  const generalState = useSelector(state => state.general);
+  const isLoading = generalState.isLoading;
+  const { children } = props;
   return (
     <div className={styles.main}>
         <Navigation />
-        <div className={styles.brand}>
-          <span className={styles.logo}>D</span>
-          <span>DeallCommurz</span>
-        </div>
-        <div className={styles.container}>
+        <div className={styles.contentContainer}>
           {children}
+          {isLoading &&
+            <div className={styles.loading}><div className={`${styles.loader} ${styles.center}`}><span></span></div></div>
+          }
         </div>
     </div>
   )
