2a7243a0536755e8a2d4fa383714d51e4281f3c7
Bagus Faizun <bagus.noorfaizun@dansmultipro.com>
2023-02-21 17:01:50 +0700

    refactor filter component

--- components/FilterOptions.js
+++ components/FilterOptions.js
@@ -1,51 +1,41 @@
 import React, { useState } from 'react'
 import styles from '@/styles/components/FilterOptions.module.css'
 import { HiPlus, HiMinus } from 'react-icons/hi'
-import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
-import { isEmpty } from 'lodash';
 
-export default function FilterOptions({name, data = [], onSelectFilter, filter}) {
-    const [expanded, setExpanded] = useState(true)
+export default function FilterOptions({data = [], onSelectFilter}) {
     const [showAll, setShowAll] = useState(false);
-    const options = showAll ? data : data.slice(0,4);
+    const options = showAll ? data : data.slice(0,15);
 
     const toggleShowAll = () => {
         setShowAll(!showAll);
     }
-
-    // console.log('clg filter', filter);
-
     
     return (
         <>
             <div className={styles.header} onClick={()=> setExpanded(!expanded)}>
-                <span className={styles.title}>{name}</span>
-                <span className={styles.expand}>{expanded ? <BiChevronUp/> : <BiChevronDown/> }</span>
+                <span className={styles.title}>Category</span>
             </div>
-            {expanded && 
-                <div className={styles.content}>
-                    {options.map(item => {
-                        const isExist = !isEmpty(filter) && filter[name];
-                        const checked = !isEmpty(isExist) && isExist.includes(item)
-                        // console.log('clg checked', checked);
-                        return (
-                            <div className={styles.filterCheckbox} key={item}>
-                                <input type='checkbox' id={`filter-${item}`} defaultChecked={checked}/>
-                                <label
-                                    onClick={() => onSelectFilter(name, item)}
-                                    htmlFor={`filter-${item}`}>
-                                    {item}
-                                </label>
-                            </div>
-                    )})}
-                    <div className={styles.showMore}>
-                        <button onClick={toggleShowAll}>
-                            {showAll ?  <HiMinus/> : <HiPlus/>}
-                            {showAll ? 'Show less' : 'Show more' }
-                        </button>
-                    </div>
+            <div className={styles.content}>
+                {options.map(item => {
+                    return (
+                        <div className={styles.filterCheckbox} key={item}>
+                            <label
+                                onClick={() => onSelectFilter(item)}
+                                htmlFor={`filter-${item}`}>
+                                {item}
+                            </label>
+                        </div>
+                )})}
+                <div className={styles.showMore}>
+                    <button onClick={toggleShowAll}>
+                        {showAll ?  
+                        <span><HiMinus/> Show less</span>
+                        : 
+                        <span><HiPlus/> Show more</span>
+                        }
+                    </button>
                 </div>
-            }
+            </div>
         </>
     )
 }
