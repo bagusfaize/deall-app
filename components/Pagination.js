2102b73e46547bf4550d564b7d94be77c0d7ee9c
Bagus Faizun <bagus.noorfaizun@dansmultipro.com>
2023-02-21 17:04:12 +0700

    adjustment pagination

--- components/Pagination.js
+++ components/Pagination.js
@@ -4,12 +4,14 @@ import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
 export default function Pagination ({page, handlePage, totalData, productLength, limit}){
     const totalPage = Math.ceil(totalData/limit);
     const disablePrev = page === 1;
-    const disableNext = productLength === totalData || page === totalPage;
+    const disableNext = page === totalPage;
     return (
-        <div className={styles.pageWrapper}>
-            <button onClick={() => handlePage('prev')} className={styles.prev} disabled={disablePrev}><GrFormPrevious/></button>
-            <span className={styles.page}>{`${page} / ${totalPage}`}</span>
-            <button onClick={() => handlePage('next')} className={styles.next} disabled={disableNext}><GrFormNext/></button>
+        <div className={styles.flexCenter}>
+            <div className={styles.pageWrapper}>
+                <button onClick={() => handlePage('prev')} className={styles.prev} disabled={disablePrev}><GrFormPrevious/></button>
+                <span className={styles.page}>{`${page} / ${totalPage}`}</span>
+                <button onClick={() => handlePage('next')} className={styles.next} disabled={disableNext}><GrFormNext/></button>
+            </div>
         </div>
     )
 }
